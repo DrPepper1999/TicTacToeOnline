d78f85000c73b7e9939f8e46661c27dd9c31b874
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-14 20:56:04 +0300

    Add DI

--- TicTacToeOnline.Application/DependencyInjection.cs
+++ TicTacToeOnline.Application/DependencyInjection.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using TicTacToeOnline.Application.Services.Authentication;
 
 namespace TicTacToeOnline.Application
 {
@@ -12,7 +7,7 @@ namespace TicTacToeOnline.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection service)
         {
-            service.AddScoped<IAuthenticationServices, AuthenticationServices>();
+            service.AddMediatR(typeof(DependencyInjection).Assembly);
 
             return service;
         }
