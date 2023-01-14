dda5e21f5e776a99e3fa18949c72fa441d121000
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-14 20:53:08 +0300

    Add Mapping(Mapster)

---
+++ TicTacToeOnline.Api/Common/Mapping/AuthenticationMappingConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mapster;
+using TicTacToeOnline.Application.Authentication.Commands.Register;
+using TicTacToeOnline.Application.Authentication.Common;
+using TicTacToeOnline.Application.Authentication.Queries.Login;
+using TicTacToeOnline.Contracts.Authentication;
+
+namespace TicTacToeOnline.Api.Common.Mapping
+{
+    public class AuthenticationMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<RegisterRequest, RegisterCommand>();
+
+            config.NewConfig<LoginRequest, LoginQuery>();
+
+            config.NewConfig<AuthenticationResult, AuthenticationResponse>()
+                .Map(dest => dest, src => src.User);
+        }
+    }
+}
---
+++ TicTacToeOnline.Api/Common/Mapping/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Mapster;
+using MapsterMapper;
+
+namespace TicTacToeOnline.Api.Common.Mapping
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddMappings(this IServiceCollection services)
+        {
+            var config = TypeAdapterConfig.GlobalSettings;
+            config.Scan(Assembly.GetExecutingAssembly());
+
+            services.AddSingleton(config);
+            services.AddScoped<IMapper, ServiceMapper>();
+            return services;
+        }
+    }
+}
