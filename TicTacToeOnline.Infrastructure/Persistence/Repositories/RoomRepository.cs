970bac9921d9778cd9573212beb0945f24e0fd26
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-21 18:12:28 +0300

    Add Two In Memory repositorys

---
+++ TicTacToeOnline.Infrastructure/Persistence/Repositories/RoomRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToeOnline.Application.Common.Interfaces.Persistence;
+using TicTacToeOnline.Domain.RoomAggregate;
+
+namespace TicTacToeOnline.Infrastructure.Persistence.Repositories
+{
+    public class RoomRepository : IRoomRepository
+    {
+        private readonly TicTacToeOnlineDbContext _dbContext;
+
+        public RoomRepository(TicTacToeOnlineDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(Room room)
+        {
+            _dbContext.Add(room);
+
+            _dbContext.SaveChanges();
+        }
+    }
+}
