a498a15a8da73cf7a3c7f58a93e5824b53775f5e
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-21 18:14:24 +0300

    Add Extemsions for convert string tp Map

---
+++ TicTacToeOnline.Application/Common/Extensions/MapExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TicTacToeOnline.Domain.GameAggregate.Enums;
+
+namespace TicTacToeOnline.Application.Common.Extensions
+{
+    public static class MapExtensions
+    {
+        public static IEnumerable<string> ConvertToString(this Mark[,] Map)
+        {
+            return Map.Cast<Mark>().Select(x => x.ToString());
+        }
+
+        public static Mark[,] ConvertStringFieldToMap(this string field)
+        {
+            return MarkMarkArray(JsonSerializer
+                .Deserialize<string[]>(field, (JsonSerializerOptions)null!)!
+                .Select(x => (Mark)Enum.Parse(typeof(Mark), x))
+                .ToArray());
+        }
+
+        private static Mark[,] MarkMarkArray(Mark[] marks)
+        {
+            var result = new Mark[3, 3];
+
+            var c = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    result[i, j] = marks[j + c];
+                }
+
+                c += 3;
+            }
+
+            return result;
+        }
+    }
+}
