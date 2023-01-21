e20f2829d6307a07819b1d787bd8547c1011e3ee
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-21 18:14:44 +0300

    Add convert string to Map

---
+++ TicTacToeOnline.Infrastructure/Persistence/Common/Conversion/MapConversion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TicTacToeOnline.Application.Common.Extensions;
+using TicTacToeOnline.Domain.GameAggregate.Enums;
+
+namespace TicTacToeOnline.Infrastructure.Persistence.Common.Conversion
+{
+    public class MapConverter : ValueConverter<Mark[,], string>
+    {
+        public MapConverter()
+            : base(
+                m => JsonSerializer
+                    .Serialize(m.ConvertToString(), (JsonSerializerOptions)null!),
+                m => m.ConvertStringFieldToMap())
+        { }
+    }
+}
---
+++ TicTacToeOnline.Infrastructure/Persistence/TicTacToeOnlineDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicTacToeOnline.Domain.RoomAggregate;
+
+namespace TicTacToeOnline.Infrastructure.Persistence
+{
+    public class TicTacToeOnlineDbContext : DbContext
+    {
+        public TicTacToeOnlineDbContext(DbContextOptions<TicTacToeOnlineDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Room> Rooms { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .ApplyConfigurationsFromAssembly(typeof(TicTacToeOnlineDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
