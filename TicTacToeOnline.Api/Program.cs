4636ac8ff033d597bafd9572b8798ba62ec81871
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-21 18:16:11 +0300

    Add nuget for JWTBearer

--- TicTacToeOnline.Api/Program.cs
+++ TicTacToeOnline.Api/Program.cs
@@ -12,16 +12,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 {
-    app.UseExceptionHandler("/error"); 
+    app.UseExceptionHandler("/error");
+
+
+    app.UseHttpsRedirection();
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+
     if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
         app.UseSwaggerUI();
     }
-
-    app.UseHttpsRedirection();
-
-    app.UseAuthorization();
 
     app.MapControllers();
 
--- TicTacToeOnline.Infrastructure/DependencyInjection.cs
+++ TicTacToeOnline.Infrastructure/DependencyInjection.cs
@@ -1,24 +1,66 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using TicTacToeOnline.Application.Common.Interfaces.Authentication;
 using TicTacToeOnline.Application.Common.Interfaces.Persistence;
 using TicTacToeOnline.Application.Common.Interfaces.Services;
 using TicTacToeOnline.Infrastructure.Authentication;
 using TicTacToeOnline.Infrastructure.Persistence;
+using TicTacToeOnline.Infrastructure.Persistence.Repositories;
 using TicTacToeOnline.Infrastructure.Services;
-
 namespace TicTacToeOnline.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection service,
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
             ConfigurationManager configuration)
         {
-            service.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
-            service.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
-            service.AddSingleton<IDataTimeProvider, DataTimeProvider>();
+            services
+                .AddAuth(configuration)
+                .AddPersistence(configuration);
 
-            service.AddScoped<IUserRepository, UserRepository>();
+            services.AddSingleton<IDataTimeProvider, DataTimeProvider>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddPersistence(this IServiceCollection services,
+            ConfigurationManager configuration)
+        {
+            services.AddDbContext<TicTacToeOnlineDbContext>(option =>
+                option.UseNpgsql());
+
+                services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IRoomRepository, RoomRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddAuth(this IServiceCollection service,
+            ConfigurationManager configuration)
+        {
+            var jwtSettings = new JwtSettings();
+            configuration.Bind(JwtSettings.SectionName, jwtSettings); // мапим конфигуарцию и объект
+
+            service.AddSingleton(Options.Create(jwtSettings));
+            service.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+
+            service.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtSettings.Secret))
+                });
 
             return service;
         }
