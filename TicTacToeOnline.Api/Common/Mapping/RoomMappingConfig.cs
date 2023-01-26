b805d29353e198582804d4e68896405eb7537804
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-26 13:55:51 +0300

    Add map Room to RoomResponse

--- TicTacToeOnline.Api/Common/Mapping/RoomMappingConfig.cs
+++ TicTacToeOnline.Api/Common/Mapping/RoomMappingConfig.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Mapster;
+using TicTacToeOnline.Application.Common.Extensions;
 using TicTacToeOnline.Application.Rooms.Commands.CreateRoom;
+using TicTacToeOnline.Application.Rooms.Queries.GetRoom;
 using TicTacToeOnline.Contracts.Room;
-using TicTacToeOnline.Domain.GameAggregate.Enums;
 using TicTacToeOnline.Domain.RoomAggregate;
+using TicTacToeOnline.Domain.RoomAggregate.Entities;
+using TicTacToeOnline.Domain.RoomAggregate.Enums;
 
 namespace TicTacToeOnline.Api.Common.Mapping
 {
@@ -20,9 +23,24 @@ namespace TicTacToeOnline.Api.Common.Mapping
                     (Mark)Enum.Parse(typeof(Mark), src.PlayerMark))
                 .Map(dest => dest, src => src);
 
-            config.NewConfig<Room, RoomResponse>()
-                .Map(dest => dest.Id, src => src.Id.Value);
+            config.NewConfig<(GetRoomRequest request, Guid roomId), GetRoomQuery>()
+                .Map(dest => dest.Id, src => src.roomId)
+                .Map(dest => dest, src => src.request);
 
+            config.NewConfig<Room, RoomResponse>()
+                .Map(dest => dest.Id, src => src.Id.Value)
+                .Map(dest => dest.Game.Map, src => src.Game.Map);
+
+            config.NewConfig<Game, GameResponse>()
+                .Map(dest => dest.Id, src => src.Id.Value)
+                .Map(dest => dest.PlayerIds, src =>
+                    src.PlayerIds.Select(x => x.Value))
+                .Map(dest => dest.PlayerTurn, src => src.PlayerTurn.Value);
+
+            config.NewConfig<Map, MapResponse>()
+                .Map(dest => dest.Id, src => src.Id.Value)
+                .Map(dest => dest.Fields, src =>
+                    string.Join(' ', src.GetFields().ConvertToString()));
         }
     }
 }
