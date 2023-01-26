235917ffe3e779bb3ebbfd8398c5a667c314098f
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-26 16:05:04 +0300

    Add UnitOfWork

---
+++ TicTacToeOnline.Application/Common/Interfaces/Persistence/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToeOnline.Domain.Common.Models;
+
+namespace TicTacToeOnline.Application.Common.Interfaces.Persistence
+{
+    public interface IRepository<T, TId>
+        where T : AggregateRoot<TId>
+        where TId : ValueObject
+    {
+        IUnitOfWork UnitOfWork { get; }
+    }
+}
