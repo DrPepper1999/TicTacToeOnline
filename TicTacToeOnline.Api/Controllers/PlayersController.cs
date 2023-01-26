c3af4ea4bd63e76387ceac874817079145b6bb6d
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-26 21:03:21 +0300

    Add PlayersController

--- TicTacToeOnline.Api/Controllers/PlayersController.cs
+++ TicTacToeOnline.Api/Controllers/PlayersController.cs
@@ -1,6 +1,8 @@
 ﻿using MapsterMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using TicTacToeOnline.Api.Hubs.TicTacToe;
 using TicTacToeOnline.Application.Players.Commands.CreatePlayer;
 using TicTacToeOnline.Contracts.Player;
 
@@ -11,22 +13,24 @@ namespace TicTacToeOnline.Api.Controllers
     {
         private readonly IMapper _mapper;
         private readonly ISender _mediator;
+        private readonly IHubContext<TicTacToeHub> _ticTacToeHubContext;
 
-        public PlayersController(IMapper mapper, ISender mediator)
+        public PlayersController(IMapper mapper, ISender mediator, IHubContext<TicTacToeHub> ticTacToeHubContext)
         {
             _mapper = mapper;
             _mediator = mediator;
+            _ticTacToeHubContext = ticTacToeHubContext;
         }
 
         [HttpPost]
         public async Task<IActionResult> Create(CreatePlayerRequest request)
         {
-            var command = _mapper.Map<CreatePlayerCommand>(request);
+            var command = _mapper.Map<CreatePlayerCommand>((request, ));
 
             var createPlayerResult = await _mediator.Send(command);
 
             return createPlayerResult.Match(
-                room => Ok(_mapper.Map<PlayerResponse>(room)), // CreatedAtAction(nameof(GetRoom), new {roomId = room.Id}, room)
+                room => Ok(_mapper.Map<PlayerResponse>(room)), // CreatedAtAction(nameof(GetPlayer), new {roomId = room.Id}, room)
                 Problem
             );
         }
