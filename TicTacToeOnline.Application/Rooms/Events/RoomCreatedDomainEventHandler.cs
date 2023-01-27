a8893075077d750579b0b451362dfd298b60d5f3
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-27 19:17:12 +0300

    Add UserCreatedDomainEvent

---
+++ TicTacToeOnline.Application/Rooms/Events/RoomCreatedDomainEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToeOnline.Domain.DomainEvents;
+
+namespace TicTacToeOnline.Application.Rooms.Events
+{
+    public class RoomCreatedDomainEventHandler : INotificationHandler<RoomCreatedDomainEvent>
+    {
+        public async Task Handle(RoomCreatedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            await Task.Delay(100, cancellationToken);
+        }
+    }
+}
