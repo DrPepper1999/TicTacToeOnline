b013b0fa7f758070bfe2dc1d6356b874c8137a4b
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-27 19:14:34 +0300

    Add IAggregateRoot for ConvertDomainEventsToOutboxMessagesInterceptor

---
+++ TicTacToeOnline.Domain/Common/Interfaces/IAggregateRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeOnline.Domain.Common.Interfaces
+{
+    public interface IAggregateRoot
+    {
+        IReadOnlyList<IDomainEvent> DomainEvents { get; }
+        void ClearDomainEvents();
+    }
+}
