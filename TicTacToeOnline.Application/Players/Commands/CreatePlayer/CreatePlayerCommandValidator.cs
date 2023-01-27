c022c91cf139b12ead5bef5c65de29f866f7e227
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-27 19:13:46 +0300

    Add SQRS create command and queary get

---
+++ TicTacToeOnline.Application/Players/Commands/CreatePlayer/CreatePlayerCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToeOnline.Application.Rooms.Commands.CreateRoom;
+
+namespace TicTacToeOnline.Application.Players.Commands.CreatePlayer
+{
+    public class CreatePlayerCommandValidator : AbstractValidator<CreatePlayerCommand>
+    {
+        public CreatePlayerCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.ConnectionId).NotEmpty();
+        }
+    }
+}
---
+++ TicTacToeOnline.Application/Players/Queries/GetPlayer/GetPlayerQuery.cs
@@ -0,0 +1,9 @@
+﻿using ErrorOr;
+using MediatR;
+using System.Linq.Expressions;
+using TicTacToeOnline.Domain.PlayerAggregate;
+
+namespace TicTacToeOnline.Application.Players.Queries.GetPlayer
+{
+    public record GetPlayerQuery(Expression<Func<Player, bool>> predicate) : IRequest<ErrorOr<Player>>;
+}
