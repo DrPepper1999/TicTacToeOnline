5b0952b74055e723cb0266de2ca98dd8b01af7b4
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-27 19:14:58 +0300

    Add Domain Event

--- TicTacToeOnline.Domain/Common/Models/AggregateRoot.cs
+++ TicTacToeOnline.Domain/Common/Models/AggregateRoot.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TicTacToeOnline.Domain.Common.Interfaces;
 
 namespace TicTacToeOnline.Domain.Common.Models
 {
-    public abstract class AggregateRoot<TId> : Entity<TId>
+    public abstract class AggregateRoot<TId> : Entity<TId>, IAggregateRoot
         where TId: notnull
     {
         protected AggregateRoot(TId id) : base(id)
         {
+        }
+
+        private readonly List<IDomainEvent> _domainEvents = new();
+
+        public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.ToList().AsReadOnly();
+
+        public void ClearDomainEvents() => _domainEvents.Clear();
+
+        protected void RaiseDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
         }
 
 #pragma warning disable CS8618
