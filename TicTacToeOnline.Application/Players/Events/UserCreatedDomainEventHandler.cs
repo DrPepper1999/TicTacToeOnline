0f8ac7b380f4688592897ec53651a5d76c3525aa
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-27 19:13:46 +0300

    Add SQRS create command and queary get

--- TicTacToeOnline.Application/Players/Events/UserCreatedDomainEventHandler.cs
+++ TicTacToeOnline.Application/Players/Events/UserCreatedDomainEventHandler.cs
@@ -1,33 +1,32 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ErrorOr;
-using MediatR;
 using TicTacToeOnline.Application.Common.Interfaces.Persistence;
+using TicTacToeOnline.Domain.Common.Errors;
+using TicTacToeOnline.Domain.DomainEvents;
 using TicTacToeOnline.Domain.PlayerAggregate;
 
-namespace TicTacToeOnline.Application.Players.Commands.CreatePlayer
+namespace TicTacToeOnline.Application.Players.Events
 {
-    public class CreatePlayerCommandHandler : IRequestHandler<CreatePlayerCommand, ErrorOr<Player>>
+    public class UserCreatedDomainEventHandler : INotificationHandler<UserCreatedDomainEvent>
     {
         private readonly IPlayerRepository _playerRepository;
 
-        public CreatePlayerCommandHandler(IPlayerRepository playerRepository)
+        public UserCreatedDomainEventHandler(IPlayerRepository playerRepository)
         {
             _playerRepository = playerRepository;
         }
 
-        public async Task<ErrorOr<Player>> Handle(CreatePlayerCommand request, CancellationToken cancellationToken)
+        public async Task Handle(UserCreatedDomainEvent notification, CancellationToken cancellationToken)
         {
-            var player = Player.Create(request.UserId, request.Name);
+            var player = Player.Create(notification.UserId, notification.Name);
 
             await _playerRepository.AddAsync(player, cancellationToken);
 
             await _playerRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
-
-            return player;
         }
     }
 }
---
+++ TicTacToeOnline.Application/Players/Queries/GetPlayer/GetPlayerQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using ErrorOr;
+using MediatR;
+using TicTacToeOnline.Application.Common.Interfaces.Persistence;
+using TicTacToeOnline.Domain.Common.Errors;
+using TicTacToeOnline.Domain.PlayerAggregate;
+
+namespace TicTacToeOnline.Application.Players.Queries.GetPlayer
+{
+    public class GetPlayerQueryHandler : IRequestHandler<GetPlayerQuery, ErrorOr<Player>>
+    {
+        private readonly IPlayerRepository _playerRepository;
+
+        public GetPlayerQueryHandler(IPlayerRepository playerRepository)
+        {
+            _playerRepository = playerRepository;
+        }
+
+        public async Task<ErrorOr<Player>> Handle(GetPlayerQuery request, CancellationToken cancellationToken)
+        {
+            var player = await _playerRepository.GetFirstWhere(request.predicate, cancellationToken);
+
+            if (player is null)
+            {
+                return Errors.Player.NotFound;
+            }
+
+            return player;
+        }
+    }
+}
