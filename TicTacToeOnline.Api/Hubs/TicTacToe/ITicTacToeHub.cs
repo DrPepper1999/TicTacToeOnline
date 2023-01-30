2874d343137ba4f138906fa0cde385ff220d407e
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-30 22:28:42 +0300

    Add Client methods setPlayer setRoom setConnectionInfo

--- TicTacToeOnline.Api/Hubs/TicTacToe/ITicTacToeHub.cs
+++ TicTacToeOnline.Api/Hubs/TicTacToe/ITicTacToeHub.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TicTacToeOnline.Contracts.Player;
+using TicTacToeOnline.Contracts.Room;
 
 namespace TicTacToeOnline.Api.Hubs.TicTacToe
 {
     public interface ITicTacToeHub
     {
+        public Task setPlayer(PlayerResponse playerResponse);
 
+        public Task setRoom(RoomResponse roomResponse);
+
+        public Task setConnectionInfo(bool isSuccessConnection, string[]? errors);
     }
 }
