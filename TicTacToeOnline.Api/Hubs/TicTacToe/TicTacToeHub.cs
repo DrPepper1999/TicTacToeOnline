7b925a2cf5962c240a375136f016f33823c6af5e
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-30 22:28:42 +0300

    Add Client methods setPlayer setRoom setConnectionInfo

--- TicTacToeOnline.Api/Hubs/TicTacToe/TicTacToeHub.cs
+++ TicTacToeOnline.Api/Hubs/TicTacToe/TicTacToeHub.cs
@@ -1,18 +1,19 @@
-﻿using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Security.Claims;
+using ErrorOr;
+using Microsoft.AspNetCore.SignalR;
 using MapsterMapper;
-using Microsoft.AspNetCore.Mvc;
-using TicTacToeOnline.Contracts.Player;
-using TicTacToeOnline.Contracts.Room;
 using MediatR;
-using TicTacToeOnline.Application.Players.Commands.CreatePlayer;
+using TicTacToeOnline.Application.Players.Queries.GetPlayer;
+using TicTacToeOnline.Application.Rooms.Commands.CreateRoom;
+using TicTacToeOnline.Contracts.Player;
+using TicTacToeOnline.Domain.UserAggregate.ValueObjects;
+using TicTacToeOnline.Contracts.Room;
+using TicTacToeOnline.Domain.Common.Errors;
+using static TicTacToeOnline.Domain.Common.Errors.Errors.Authentication;
 
 namespace TicTacToeOnline.Api.Hubs.TicTacToe
 {
+
     public class TicTacToeHub : Hub<ITicTacToeHub>
     {
         private readonly IMapper _mapper;
@@ -25,43 +26,67 @@ namespace TicTacToeOnline.Api.Hubs.TicTacToe
             _mediator = mediator;
         }
 
-        public override Task OnConnectedAsync()
+        public async Task CreateRoomAuth(CreateRoomRequest createRoomHubRequest)
         {
-            IsAuthenticated = Context.User?.Identity?.IsAuthenticated ?? false;
-
-            if (!IsAuthenticated)
+            // if user authenticated
+            var userIdentity = (Context.User!.Identity as ClaimsIdentity);
+            if ((userIdentity?.IsAuthenticated ?? false) is false)
             {
-                return base.OnConnectedAsync();
+                await Clients.Caller.setConnectionInfo(false,
+                    new[] { InvalidCredentials.Description });
+                return;
             }
+            var userId = GetUserId(userIdentity);
 
+            var getPlayerQuery = new GetPlayerQuery(x =>
+                x.UserId! == userId);
 
+            var playerResult = await _mediator.Send(getPlayerQuery);
 
-            return base.OnConnectedAsync();
-        }
-
-        public async Task CreateRoom(CreatePlayerRequest createPlayerRequest, CreateRoomRequest createRoomRequest)
-        {
-            // if user not authenticated
-            var createPlayerCommand = _mapper.Map<CreatePlayerCommand>(createPlayerRequest);
-
-            var createPlayerResult = await _mediator.Send(createPlayerCommand);
-
-            if (createPlayerResult.IsError)
+            if (playerResult.IsError)
             {
-
+                await Clients.Caller.setConnectionInfo(false,
+                    playerResult.Errors.Select(x => x.Description).ToArray());
+                return;
             }
+            playerResult.Value.AppendConnection(Context.ConnectionId);
+            await Clients.Caller.setPlayer(_mapper.Map<PlayerResponse>(playerResult.Value));
 
             var createRoomCommand = _mapper
-                .Map<CreatePlayerCommand>((createRoomRequest, createPlayerResult.Value.Id));
+               .Map<CreateRoomCommand>((createRoomHubRequest, playerResult.Value.Id));
 
             var createRoomResult = await _mediator.Send(createRoomCommand);
 
             if (createRoomResult.IsError)
             {
-
+                await Clients.Caller.setConnectionInfo(false,
+                    createRoomResult.Errors.Select(x => x.Description).ToArray());
+                return;
             }
 
-            //Clients.Caller
+            await Clients.Caller.setRoom(_mapper.Map<RoomResponse>(createRoomResult.Value));
+
+            await Groups
+                .AddToGroupAsync(Context.ConnectionId, createRoomResult.Value.Id.Value.ToString());
+
+            await Clients.Caller.setConnectionInfo(true, null);
+        }
+
+        public async Task CreateRoom(CreatePlayerRequest createPlayerRequest,
+            CreateRoomRequest createRoomHubRequest)
+        {
+            await Task.Delay(100);
+        }
+
+        public async Task JoinRoomAuth()
+        {
+
+        }
+
+        private static UserId GetUserId(ClaimsIdentity userIdentity)
+        {
+            return UserId.Create(Guid.Parse(userIdentity!
+                .FindFirst(ClaimTypes.NameIdentifier)!.Value));
         }
     }
 }
