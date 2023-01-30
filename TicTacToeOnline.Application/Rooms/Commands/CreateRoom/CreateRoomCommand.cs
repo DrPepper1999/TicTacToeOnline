bf77bb9cc3ca78d755355ceff13c785138e00f05
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-01-30 22:33:44 +0300

    Add new properties PlayersForStart MapSize

--- TicTacToeOnline.Application/Rooms/Commands/CreateRoom/CreateRoomCommand.cs
+++ TicTacToeOnline.Application/Rooms/Commands/CreateRoom/CreateRoomCommand.cs
@@ -9,6 +9,7 @@ namespace TicTacToeOnline.Application.Rooms.Commands.CreateRoom
     public record CreateRoomCommand(
         string Name,
         string? Password,
-        int? PlayersForStart,
+        int PlayersForStart,
+        int MapSize,
         PlayerId PlayerId) : IRequest<ErrorOr<Room>>;
 }
