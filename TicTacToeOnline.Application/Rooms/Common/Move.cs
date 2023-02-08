ccbd28ee4447941826e2564adca144dc223c5a80
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:53:48 +0300

    Add Move model

---
+++ TicTacToeOnline.Application/Rooms/Common/Move.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+using TicTacToeOnline.Domain.Common.Enums;
+using TicTacToeOnline.Domain.GameAggregate.ValueObjects;
+using TicTacToeOnline.Domain.TeamAggregate.ValueObjects;
+
+namespace TicTacToeOnline.Application.Rooms.Common
+{
+    public record Move(Point PlayerMove, Mark Mark, TeamId TeamId, GameId GameId);
+}
