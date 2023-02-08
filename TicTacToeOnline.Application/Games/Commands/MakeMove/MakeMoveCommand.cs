d37d5b0a05c89a51b88f34faf00a14d72685d1e3
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:06:25 +0300

    replase command

---
+++ TicTacToeOnline.Application/Games/Commands/MakeMove/MakeMoveCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+using ErrorOr;
+using MediatR;
+using TicTacToeOnline.Application.Rooms.Common;
+using TicTacToeOnline.Domain.Common.Enums;
+using TicTacToeOnline.Domain.GameAggregate.ValueObjects;
+using TicTacToeOnline.Domain.TeamAggregate.ValueObjects;
+
+namespace TicTacToeOnline.Application.Games.Commands.MakeMove
+{
+    public record MakeMoveCommand(Point Move, TeamId TeamId, Mark Mark, GameId GameId) : IRequest<ErrorOr<Move>>;
+}
---
+++ TicTacToeOnline.Application/Games/Commands/MakeMove/MakeMoveCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using TicTacToeOnline.Domain.Common.Enums;
+
+namespace TicTacToeOnline.Application.Games.Commands.MakeMove
+{
+    public class MakeMoveCommandValidator : AbstractValidator<MakeMoveCommand>
+    {
+        public MakeMoveCommandValidator()
+        {
+            RuleFor(x => x.Move).NotEmpty();
+            RuleFor(x => x.TeamId).NotEmpty();
+            RuleFor(x => x.GameId).NotEmpty();
+            RuleFor(x => x.Mark)
+                .NotEmpty()
+                .IsInEnum()
+                .Must(x => x != Mark.Empty);
+        }
+    }
+}
