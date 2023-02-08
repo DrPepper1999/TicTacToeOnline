d2c718585a2a204cdac35ea7532c1d507cdd7f4c
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:58:00 +0300

    Add RefreshToken

--- TicTacToeOnline.Api/Controllers/AuthenticationController.cs
+++ TicTacToeOnline.Api/Controllers/AuthenticationController.cs
@@ -1,9 +1,13 @@
-﻿using MapsterMapper;
+﻿using ErrorOr;
+using MapsterMapper;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TicTacToeOnline.Api.Common.Cookies;
+using TicTacToeOnline.Application.Authentication.Commands.CreateRefreshToken;
 using TicTacToeOnline.Application.Authentication.Commands.Register;
 using TicTacToeOnline.Application.Authentication.Common;
+using TicTacToeOnline.Application.Authentication.Queries.GetToken;
 using TicTacToeOnline.Application.Authentication.Queries.Login;
 using TicTacToeOnline.Contracts.Authentication;
 using TicTacToeOnline.Domain.Common.Errors;
@@ -30,7 +34,7 @@ namespace TicTacToeOnline.Api.Controllers
 
             var test = _mapper.Map<AuthenticationResponse>(authResult);
            return authResult.Match(
-                authResult => Ok(_mapper.Map<AuthenticationResponse>(authResult)),
+                auth => Ok(_mapper.Map<AuthenticationResponse>(auth)),
                 Problem);
         }
 
@@ -47,10 +51,76 @@ namespace TicTacToeOnline.Api.Controllers
                     title: authResult.FirstError.Description);
             }
 
+            var command = new CreateRefreshTokenCommand(authResult.Value.User.Id);
+
+            var refreshToken = await _mediator.Send(command);
+
+            if (refreshToken.IsError)
+            {
+                return Problem(refreshToken.Errors);
+            }
+
+            SetRefreshToken(refreshToken.Value);
+
             return authResult.Match(
-                authResult => Ok(_mapper.Map<AuthenticationResponse>(authResult)),
+                auth => Ok(_mapper.Map<AuthenticationResponse>(auth)),
                 Problem 
             );
+        }
+
+        [HttpPost("refresh")]
+        public async Task<IActionResult> Refresh()
+        {
+            var refreshToken = Request.Cookies[CookieItemKeys.RefreshToken];
+
+            if (refreshToken is null)
+            {
+               return Unauthorized("Cookies doesn't has Refresh Token");
+            }
+
+            var tokenResult = await GetTokenByRefreshToken(refreshToken);
+
+            if (tokenResult.IsError)
+            {
+                return Problem(tokenResult.Errors);
+            }
+
+            var refreshTokenResult = await _mediator
+                .Send(new CreateRefreshTokenCommand(tokenResult.Value.UserId));
+
+            if (refreshTokenResult.IsError)
+            {
+                return Problem(refreshTokenResult.Errors);
+            }
+
+            SetRefreshToken(refreshTokenResult.Value);
+
+            return Ok(tokenResult);
+        }
+
+        private async Task<ErrorOr<TokenResult>> GetTokenByRefreshToken(string? refreshToken)
+        {
+            var query = new GetTokenByRefreshTokenQuery(refreshToken!);
+
+            var tokenResult = await _mediator.Send(query);
+
+            return tokenResult;
+        }
+
+
+        private void SetRefreshToken(RefreshTokenResult refreshTokenResult)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                SameSite = SameSiteMode.None,
+                Secure = true,
+                Expires = refreshTokenResult.Expires
+            };
+
+            Response
+                .Cookies
+                .Append(CookieItemKeys.RefreshToken, refreshTokenResult.RefreshToken, cookieOptions);
         }
     }
 }
