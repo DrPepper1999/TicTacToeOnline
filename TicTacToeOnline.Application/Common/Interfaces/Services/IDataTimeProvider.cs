853756ea54b0096847e2d4399123a732ec5b67fd
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:51:56 +0300

    Add ExpiryMinutesRefreshToken

--- TicTacToeOnline.Application/Common/Interfaces/Services/IDataTimeProvider.cs
+++ TicTacToeOnline.Application/Common/Interfaces/Services/IDataTimeProvider.cs
@@ -9,5 +9,6 @@ namespace TicTacToeOnline.Application.Common.Interfaces.Services
     public interface IDataTimeProvider
     {
         DateTime UtcNow { get; }
+        DateTime ExpiryMinutesRefreshToken { get; }
     }
 }
