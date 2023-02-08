94745d375d4ab2693595285c242fe83cce572221
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:57:24 +0300

    Add OutboxMessageConsumer

---
+++ TicTacToeOnline.Infrastructure/Persistence/Outbox/OutboxMessageConsumer.cs
@@ -0,0 +1,9 @@
+﻿namespace TicTacToeOnline.Infrastructure.Persistence.Outbox
+{
+    public sealed class OutboxMessageConsumer
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
--- TicTacToeOnline.Infrastructure/Persistence/TicTacToeOnlineDbContext.cs
+++ TicTacToeOnline.Infrastructure/Persistence/TicTacToeOnlineDbContext.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
 using TicTacToeOnline.Application.Common.Interfaces.Persistence;
+using TicTacToeOnline.Domain.Common.Models;
+using TicTacToeOnline.Domain.GameAggregate;
 using TicTacToeOnline.Domain.PlayerAggregate;
 using TicTacToeOnline.Domain.RoomAggregate;
+using TicTacToeOnline.Domain.TeamAggregate;
 using TicTacToeOnline.Domain.UserAggregate;
+using TicTacToeOnline.Infrastructure.Persistence.Configurations;
 using TicTacToeOnline.Infrastructure.Persistence.Outbox;
 
 namespace TicTacToeOnline.Infrastructure.Persistence
@@ -14,17 +19,28 @@ namespace TicTacToeOnline.Infrastructure.Persistence
         {
         }
 
+        public DbSet<Team> Teams { get; set; } = null!;
         public DbSet<Room> Rooms { get; set; } = null!;
+        public DbSet<Game> Games { get; set; } = null!;
         public DbSet<Player> Players { get; set; } = null!;
         public DbSet<OutboxMessage> OutBoxMessages { get; set; } = null!;
+        public DbSet<OutboxMessageConsumer> OutboxMessageConsumers { get; set; } = null!;
         public DbSet<User> User { get; set; } = null!;
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            modelBuilder
-                .ApplyConfigurationsFromAssembly(typeof(TicTacToeOnlineDbContext).Assembly);
+            //builder
+                //.ApplyConfigurationsFromAssembly(GetType().Assembly)
 
-            base.OnModelCreating(modelBuilder);
+            builder.ApplyConfiguration(new UserConfigurations());
+            builder.ApplyConfiguration(new PlayerConfigurations());
+            builder.ApplyConfiguration(new TeamConfigurations());
+            builder.ApplyConfiguration(new RoomConfigurations());
+            builder.ApplyConfiguration(new GameConfigurations());
+            builder.ApplyConfiguration(new OutboxMessageConfiguration());
+            builder.ApplyConfiguration(new OutboxMessageConsumerConfiguration());
+
+            base.OnModelCreating(builder);
         }
     }
 }
