480c3efee05b1997f321425547076e0a6916c3b9
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:53:16 +0300

    Add ExpiryMinutesRefreshToken

--- TicTacToeOnline.Application/Common/Interfaces/Services/IDataTimeProvider.cs
+++ TicTacToeOnline.Application/Common/Interfaces/Services/IDataTimeProvider.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TicTacToeOnline.Application.Common.Interfaces.Services
+﻿namespace TicTacToeOnline.Application.Common.Interfaces.Services
 {
     public interface IDataTimeProvider
     {
