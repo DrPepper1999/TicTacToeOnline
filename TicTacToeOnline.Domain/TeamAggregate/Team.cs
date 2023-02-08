88662f83e4e4f67cc16c00dcc3ce873bacd8ed86
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:09:59 +0300

    Add Aggregate Team

---
+++ TicTacToeOnline.Domain/TeamAggregate/Team.cs
@@ -0,0 +1,42 @@
+﻿using TicTacToeOnline.Domain.Common.Enums;
+using TicTacToeOnline.Domain.Common.Models;
+using TicTacToeOnline.Domain.PlayerAggregate.ValueObjects;
+using TicTacToeOnline.Domain.TeamAggregate.ValueObjects;
+
+namespace TicTacToeOnline.Domain.TeamAggregate
+{
+    public class Team : AggregateRoot<TeamId>
+    {
+        private readonly List<PlayerId> _playerIds = new();
+
+        public Mark Mark { get; private set; }
+        public int Score { get; private set; }
+        public IReadOnlyList<PlayerId> PlayerIds => _playerIds.AsReadOnly();
+
+        public DateTime CreatedDateTime { get; private set; }
+        public DateTime UpdateDateTime { get; private set; }
+
+        private Team(TeamId id)
+            : base(id)
+        {
+            Mark = Mark.Empty;
+            Score = 0;
+        }
+
+        public static Team Create()
+        {
+            return new Team(TeamId.CreateUnique());
+        }
+
+        public void AddPlayer(PlayerId playerId)
+        {
+            //_playerIds.Add(playerId);
+        }
+
+#pragma warning disable CS8618
+        private Team()
+        {
+        }
+#pragma warning disable CS8618
+    }
+}
