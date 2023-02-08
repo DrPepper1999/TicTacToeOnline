9ebc616880ed7de04e0c547a9875ab67ec31b7a2
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:27:41 +0300

    Add ExpiryMinutesRefreshToken

--- TicTacToeOnline.Infrastructure/Services/DataTimeProvider.cs
+++ TicTacToeOnline.Infrastructure/Services/DataTimeProvider.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 using TicTacToeOnline.Application.Common.Interfaces.Services;
+using TicTacToeOnline.Infrastructure.Authentication.JwtToken;
+using TicTacToeOnline.Infrastructure.Authentication.RefreshToken;
 
 namespace TicTacToeOnline.Infrastructure.Services
 {
     public class DataTimeProvider : IDataTimeProvider
     {
+        private readonly RefreshTokenSettings _refreshTokenSettings;
+
+        public DataTimeProvider(IOptions<RefreshTokenSettings> refreshTokenSettings)
+        {
+            _refreshTokenSettings = refreshTokenSettings.Value;
+        }
+
         public DateTime UtcNow => DateTime.UtcNow;
+        public DateTime ExpiryMinutesRefreshToken =>
+            UtcNow.AddMinutes(_refreshTokenSettings.ExpiryMinutes);
     }
 }
