25884db58c142f2208a507282a92f79fff476530
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:56:28 +0300

    Add RefreshToken Generator and Setting

---
+++ TicTacToeOnline.Infrastructure/Authentication/RefreshToken/RefreshTokenGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+using TicTacToeOnline.Application.Common.Interfaces.Authentication;
+
+namespace TicTacToeOnline.Infrastructure.Authentication.RefreshToken
+{
+    public class RefreshTokenGenerator : IRefreshTokenGenerator
+    {
+        public string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[32];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+    }
+}
---
+++ TicTacToeOnline.Infrastructure/Authentication/RefreshToken/RefreshTokenSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace TicTacToeOnline.Infrastructure.Authentication.RefreshToken
+{
+    public class RefreshTokenSettings
+    {
+        public const string SectionName = "RefreshTokenSettings";
+        public int ExpiryMinutes { get; init; }
+    }
+}
