9f58c430692987d2d841477b8dd99bf0b924eecf
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:10:45 +0300

    Add Aggregate Game

---
+++ TicTacToeOnline.Domain/GameAggregate/ValueObjects/GameId.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using TicTacToeOnline.Domain.Common.Models;
+
+namespace TicTacToeOnline.Domain.GameAggregate.ValueObjects
+{
+    public sealed class GameId : ValueObject
+    {
+        public Guid Value { get; }
+
+        [JsonConstructor]
+        private GameId(Guid value)
+        {
+            Value = value;
+        }
+
+        public static GameId CreateUnique()
+        {
+            return new(Guid.NewGuid());
+        }
+
+        public static GameId Create(Guid gameId)
+        {
+            return new(gameId);
+        }
+
+        public override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
---
+++ TicTacToeOnline.Domain/GameAggregate/ValueObjects/MapId.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using TicTacToeOnline.Domain.Common.Models;
+
+namespace TicTacToeOnline.Domain.RoomAggregate.ValueObjects
+{
+    public sealed class MapId : ValueObject
+    {
+        public Guid Value { get; private set; }
+
+        [JsonConstructor]
+        private MapId(Guid value)
+        {
+            Value = value;
+        }
+
+        public override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+
+        public static MapId Create(Guid value)
+        {
+            return new MapId(value);
+        }
+
+        public static MapId CreateUnique()
+        {
+            return new MapId(Guid.NewGuid());
+        }
+    }
+}
