8bd99db4602ea6936ade668d51f9f1dc86aa0e37
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:51:42 +0300

    Add IDomainEventHandler

---
+++ TicTacToeOnline.Application/Common/Interfaces/Messaging/IDomainEventHandler.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TicTacToeOnline.Domain.Common.Interfaces;
+
+namespace TicTacToeOnline.Application.Common.Interfaces.Messaging
+{
+    public interface IDomainEventHandler<in TEvent> : INotificationHandler<TEvent>
+        where TEvent : IDomainEvent
+    {
+    }
+}
