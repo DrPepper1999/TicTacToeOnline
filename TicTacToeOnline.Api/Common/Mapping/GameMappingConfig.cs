c824ff2ff069ec15211216f17571fbb6c3a5115d
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:49:57 +0300

    Add Mapping for Player and Game

---
+++ TicTacToeOnline.Api/Common/Mapping/GameMappingConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using Mapster;
+using TicTacToeOnline.Application.Games.Commands.MakeMove;
+using TicTacToeOnline.Application.Rooms.Common;
+using TicTacToeOnline.Contracts.Room;
+using TicTacToeOnline.Domain.Common.Enums;
+using TicTacToeOnline.Domain.RoomAggregate.ValueObjects;
+using TicTacToeOnline.Domain.TeamAggregate.ValueObjects;
+
+namespace TicTacToeOnline.Api.Common.Mapping
+{
+    public class GameMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<MoveRequest, MakeMoveCommand>()
+                .Map(dest => dest.Move,
+                    src => new Point(src.X, src.Y))
+                .Map(dest => dest.TeamId,
+                    src => TeamId.Create(Guid.Parse(src.TeamId)))
+                .Map(dest => dest.Mark,
+                    src => (Mark)Enum.Parse(typeof(Mark), src.Mark))
+                .Map(dest => dest.GameId,
+                    src => RoomId.Create(Guid.Parse(src.GameId)));
+
+            config.NewConfig<Move, MoveResponse>()
+                .Map(dest => dest.X, src => src.PlayerMove.X)
+                .Map(dest => dest.Y, src => src.PlayerMove.Y)
+                .Map(dest => dest.Mark, src => src.Mark.ToString())
+                .Map(dest => dest.TeamId, src => src.TeamId.Value);
+        }
+    }
+}
--- TicTacToeOnline.Api/Common/Mapping/RoomMappingConfig.cs
+++ TicTacToeOnline.Api/Common/Mapping/RoomMappingConfig.cs
@@ -1,12 +1,11 @@
 ﻿using Mapster;
-using TicTacToeOnline.Application.Common.Extensions;
 using TicTacToeOnline.Application.Rooms.Commands.CreateRoom;
 using TicTacToeOnline.Application.Rooms.Commands.DeleteRoom;
 using TicTacToeOnline.Application.Rooms.Queries.GetRoom;
 using TicTacToeOnline.Contracts.Room;
+using TicTacToeOnline.Domain.Common.ValueObjects;
 using TicTacToeOnline.Domain.PlayerAggregate.ValueObjects;
 using TicTacToeOnline.Domain.RoomAggregate;
-using TicTacToeOnline.Domain.RoomAggregate.Entities;
 
 namespace TicTacToeOnline.Api.Common.Mapping
 {
@@ -14,8 +13,9 @@ namespace TicTacToeOnline.Api.Common.Mapping
     {
         public void Register(TypeAdapterConfig config)
         {
-            config.NewConfig<(CreateRoomRequest request, string playerId), CreateRoomCommand>()
-                .Map(dest => dest.PlayerId, src => PlayerId.Create(Guid.Parse(src.playerId)))
+            config.NewConfig<(CreateRoomRequest request, Guid playerId), CreateRoomCommand>()
+                .Map(dest => dest.PlayerId,
+                    src => src.playerId)
                 .Map(dest => dest, src => src.request);
 
             config.NewConfig<(CreateRoomRequest request, PlayerId playerId), CreateRoomCommand>()
@@ -32,18 +32,15 @@ namespace TicTacToeOnline.Api.Common.Mapping
 
             config.NewConfig<Room, RoomResponse>()
                 .Map(dest => dest.Id, src => src.Id.Value)
-                .Map(dest => dest.Game.Map, src => src.Game.Map);
+                .Map(dest => dest.Status, src => src.Status.ToString())
+                .Map(dest => dest.MapSize, src => src.GameSetting.MapSize)
+                .Map(dest => dest.MaxPlayers, src => src.GameSetting.MaxPlayers)
+                .Map(dest => dest.TeamIds,
+                src => src.TeamIds.Select(x => x.Value))
+                .Map(dest => dest.PlayerIds,
+                src => src.PlayerIds.Select(x => x.Value));
 
-            config.NewConfig<Game, GameResponse>()
-                .Map(dest => dest.Id, src => src.Id.Value)
-                .Map(dest => dest.PlayerIds, src =>
-                    src.PlayerIds.Select(x => x.Value))
-                .Map(dest => dest.PlayerTurn, src => src.PlayerTurn.Value);
 
-            config.NewConfig<Map, MapResponse>()
-                .Map(dest => dest.Id, src => src.Id.Value)
-                .Map(dest => dest.Fields, src =>
-                    string.Join(' ', src.GetFields().ConvertToString()));
         }
     }
 }
