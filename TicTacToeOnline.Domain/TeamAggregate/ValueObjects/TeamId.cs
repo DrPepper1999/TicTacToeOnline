4dd695743616983c3013d7b191f302d4cae5e932
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:09:59 +0300

    Add Aggregate Team

---
+++ TicTacToeOnline.Domain/TeamAggregate/ValueObjects/TeamId.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using TicTacToeOnline.Domain.Common.Models;
+
+namespace TicTacToeOnline.Domain.TeamAggregate.ValueObjects
+{
+    public class TeamId : ValueObject
+    {
+        public Guid Value { get; }
+
+        [JsonConstructor]
+        private TeamId(Guid value)
+        {
+            Value = value;
+        }
+
+        public static TeamId CreateUnique()
+        {
+            return new TeamId(Guid.NewGuid());
+        }
+
+        public static TeamId Create(Guid teamId)
+        {
+            return new TeamId(teamId);
+        }
+
+        public override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
