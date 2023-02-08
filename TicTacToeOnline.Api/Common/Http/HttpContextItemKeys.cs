a900e79ce2ecac189af4ee7e4b50a6327ad3c824
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:49:01 +0300

    not changes

--- TicTacToeOnline.Api/Common/Http/HttpContextItemKeys.cs
+++ TicTacToeOnline.Api/Common/Http/HttpContextItemKeys.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TicTacToeOnline.Api.Common.Http
+﻿namespace TicTacToeOnline.Api.Common.Http
 {
     public static class HttpContextItemKeys
     {
