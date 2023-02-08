5903692677d74638d9730753c5b136384ed803e0
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:54:42 +0300

    Add CreateRoomRequest

--- TicTacToeOnline.Contracts/Room/CreateRoomRequest.cs
+++ TicTacToeOnline.Contracts/Room/CreateRoomRequest.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TicTacToeOnline.Contracts.Room
+﻿namespace TicTacToeOnline.Contracts.Room
 {
     public record CreateRoomRequest(
         string Name,
         string? Password,
-        int PlayersForStart,
-        int MapSize
+        int MaxPlayers,
+        int MapSize,
+        int TeamCount
     );
 }
