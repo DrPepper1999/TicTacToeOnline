434b2f573990c977bdd2461bd9b56f715e9d9589
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:06:25 +0300

    replase command

---
+++ TicTacToeOnline.Application/Games/Commands/MakeMove/MakeMoveCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using ErrorOr;
+using MediatR;
+using TicTacToeOnline.Application.Common.Interfaces.Persistence;
+using TicTacToeOnline.Application.Rooms.Common;
+using TicTacToeOnline.Domain.Common.Errors;
+
+namespace TicTacToeOnline.Application.Games.Commands.MakeMove
+{
+    public class MakeMoveCommandHandler : IRequestHandler<MakeMoveCommand, ErrorOr<Move>>
+    {
+        private readonly IGameRepository _gameRepository;
+
+        public MakeMoveCommandHandler(IGameRepository gameRepository)
+        {
+            _gameRepository = gameRepository;
+        }
+
+        public async Task<ErrorOr<Move>> Handle(MakeMoveCommand request, CancellationToken cancellationToken)
+        {
+            var game = await _gameRepository.GetByIdAsync(request.GameId.Value, cancellationToken);
+
+            if (game is null)
+            {
+                return Errors.Game.NotFound;
+            }
+
+            if (request.TeamId != game.PlayerTurn)
+            {
+                return Errors.Game.AnotherTeamTurn;
+            }
+
+            var makeMoveResult = game.MakeMove(request.Move, request.Mark);
+
+            if (makeMoveResult.HasValue)
+            {
+                return makeMoveResult.Value;
+            }
+
+            return new Move(request.Move, request.Mark, request.TeamId, request.GameId);
+        }
+    }
+}
