4c3a71cf73064208d3d0cfb67112ac99718b7e86
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:27:15 +0300

    Add Aggregates Repositor

---
+++ TicTacToeOnline.Infrastructure/Persistence/Repositories/TeamRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicTacToeOnline.Application.Common.Interfaces.Persistence;
+using TicTacToeOnline.Domain.RoomAggregate.ValueObjects;
+using TicTacToeOnline.Domain.TeamAggregate;
+using TicTacToeOnline.Domain.TeamAggregate.ValueObjects;
+
+namespace TicTacToeOnline.Infrastructure.Persistence.Repositories
+{
+    public class TeamRepository : ITeamRepository
+    {
+        private readonly TicTacToeOnlineDbContext _dbContext;
+
+        public TeamRepository(TicTacToeOnlineDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IUnitOfWork UnitOfWork => _dbContext;
+        public async Task AddAsync(Team team, CancellationToken cancellationToken = default)
+        {
+            await _dbContext.AddAsync(team, cancellationToken);
+        }
+
+        public async Task<Team?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            var entity = await _dbContext.Teams
+                .FirstOrDefaultAsync(x => x.Id == TeamId.Create(id), cancellationToken);
+
+            return entity;
+        }
+
+        public async Task DeleteAsync(Team team)
+        {
+            await Task.CompletedTask;
+
+            //_dbContext.Teams.Remove(team);
+        }
+    }
+}
--- TicTacToeOnline.Infrastructure/Persistence/Repositories/UserRepository.cs
+++ TicTacToeOnline.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 using TicTacToeOnline.Application.Common.Interfaces.Persistence;
 using TicTacToeOnline.Domain.UserAggregate;
+using TicTacToeOnline.Domain.UserAggregate.ValueObjects;
 
 namespace TicTacToeOnline.Infrastructure.Persistence.Repositories
 {
@@ -15,6 +17,17 @@ namespace TicTacToeOnline.Infrastructure.Persistence.Repositories
 
         public IUnitOfWork UnitOfWork => _dbContext;
 
+        public async Task<User?> GetUserById(UserId userId)
+        {
+            return await _dbContext.User.SingleOrDefaultAsync(u => u.Id == userId);
+        }
+
+        public async Task<User?> GetFirstWhere(Expression<Func<User, bool>> predicate,
+            CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.User.FirstOrDefaultAsync(predicate, cancellationToken);
+        }
+
         public async Task<User?> GetUserByEmail(string email)
         {
             return await _dbContext.User.SingleOrDefaultAsync(u => u.Email == email);
@@ -23,6 +36,13 @@ namespace TicTacToeOnline.Infrastructure.Persistence.Repositories
         public async Task Add(User user)
         {
            await _dbContext.User.AddAsync(user);
+        }
+
+        public async Task UpdateAsync(User entity)
+        {
+            await Task.CompletedTask;
+
+            _dbContext.Entry(entity).State = EntityState.Modified;
         }
     }
 }
