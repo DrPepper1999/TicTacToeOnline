60af59796ad75bb63724281891e5831810e8c45a
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-08 18:26:19 +0300

    Add URI ProfileImage

--- TicTacToeOnline.Domain/PlayerAggregate/Player.cs
+++ TicTacToeOnline.Domain/PlayerAggregate/Player.cs
@@ -1,10 +1,7 @@
-﻿using ErrorOr;
-using TicTacToeOnline.Domain.Common.Errors;
-using TicTacToeOnline.Domain.Common.Models;
+﻿using TicTacToeOnline.Domain.Common.Models;
 using TicTacToeOnline.Domain.Common.ValueObjects;
 using TicTacToeOnline.Domain.DomainEvents;
 using TicTacToeOnline.Domain.PlayerAggregate.ValueObjects;
-using TicTacToeOnline.Domain.RoomAggregate.Enums;
 using TicTacToeOnline.Domain.UserAggregate.ValueObjects;
 
 namespace TicTacToeOnline.Domain.PlayerAggregate
@@ -14,8 +11,8 @@ namespace TicTacToeOnline.Domain.PlayerAggregate
         private readonly List<ConnectionInfo> _connections = new();
 
         public string Name { get; private set; }
-        public Mark Mark { get; private set; }
         public AverageRating AverageRating { get; private set; }
+        public Uri? ProfileImage { get; private set; }
         public UserId? UserId { get; private set; }
 
         public IReadOnlyList<ConnectionInfo> Connections => _connections.AsReadOnly();
@@ -27,13 +24,11 @@ namespace TicTacToeOnline.Domain.PlayerAggregate
             PlayerId id,
             UserId? userId,
             string name,
-            Mark mark,
             AverageRating averageRating)
             : base(id)
         {
             UserId = userId;
             Name = name;
-            Mark = mark;
             AverageRating = averageRating;
         }
 
@@ -42,17 +37,7 @@ namespace TicTacToeOnline.Domain.PlayerAggregate
             return new Player(
                 PlayerId.CreateUnique(),
                 userId, name,
-                Mark.Empty,
                 AverageRating.CreateNew());
-        }
-
-        public Error? SetMark(Mark mark)
-        {
-            if (mark == Mark.Empty) return Errors.Player.MarkCannotBeEmpty;
-
-            Mark = mark;
-
-            return null;
         }
 
         public void AppendConnection(string connectionId)
@@ -66,7 +51,7 @@ namespace TicTacToeOnline.Domain.PlayerAggregate
 
             _connections.Add(connection);
 
-            RaiseDomainEvent(new PlayerConnectionAppendDomainEvent(this));
+            RaiseDomainEvent(new PlayerConnectionAppendDomainEvent(Guid.NewGuid(), this));
         }
 
         /// <summary>
