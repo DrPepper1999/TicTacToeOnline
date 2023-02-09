e5c89eb389ba0b66402ace21043d3cfa3b6411f0
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-10 01:08:01 +0300

    delete domain event

--- TicTacToeOnline.Domain/DomainEvents/RoomCreatedDomainEvent.cs
+++
@@ -1,7 +0,0 @@
-﻿using TicTacToeOnline.Domain.Common.Interfaces;
-using TicTacToeOnline.Domain.RoomAggregate.ValueObjects;
-
-namespace TicTacToeOnline.Domain.DomainEvents
-{
-    public sealed record RoomCreatedDomainEvent(Guid Id, RoomId RoomId) : IDomainEvent;
-}
