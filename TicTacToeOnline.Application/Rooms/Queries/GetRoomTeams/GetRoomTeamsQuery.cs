06818f907a3f85a5ec3f09e05e07e360dd71e3ae
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-10 01:07:33 +0300

    Add GetRoomTeamsQuery

---
+++ TicTacToeOnline.Application/Rooms/Queries/GetRoomTeams/GetRoomTeamsQuery.cs
@@ -0,0 +1,8 @@
+﻿using ErrorOr;
+using MediatR;
+using TicTacToeOnline.Domain.TeamAggregate;
+
+namespace TicTacToeOnline.Application.Rooms.Queries.GetTeams
+{
+    public record GetRoomTeamsQuery(Guid RoomId) : IRequest<ErrorOr<IEnumerable<Team>>>;
+}
---
+++ TicTacToeOnline.Application/Rooms/Queries/GetRoomTeams/GetRoomTeamsQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using ErrorOr;
+using MediatR;
+using TicTacToeOnline.Application.Common.Interfaces.Persistence;
+using TicTacToeOnline.Domain.Common.Errors;
+using TicTacToeOnline.Domain.TeamAggregate;
+
+namespace TicTacToeOnline.Application.Rooms.Queries.GetTeams
+{
+    public class GetRoomTeamsQueryHandler : IRequestHandler<GetRoomTeamsQuery, ErrorOr<IEnumerable<Team>>>
+    {
+        private readonly IRoomRepository _roomRepository;
+        private readonly ITeamRepository _teamRepository;
+
+        public GetRoomTeamsQueryHandler(IRoomRepository roomRepository, ITeamRepository teamRepository)
+        {
+            _roomRepository = roomRepository;
+            _teamRepository = teamRepository;
+        }
+
+        public async Task<ErrorOr<IEnumerable<Team>>> Handle(GetRoomTeamsQuery request, CancellationToken cancellationToken)
+        {
+            var room = await _roomRepository.GetByIdAsync(request.RoomId, cancellationToken);
+
+            if (room is null)
+            {
+                return Errors.Room.NotFound;
+            }
+
+            var teams = await _teamRepository
+                .GetRangeByIdsAsync(room.TeamIds.ToList(), cancellationToken);
+
+           return ErrorOrFactory
+               .From(teams);
+        }
+    }
+}
