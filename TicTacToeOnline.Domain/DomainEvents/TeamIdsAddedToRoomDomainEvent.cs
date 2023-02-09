18ede37b9ace38af5aec2abaed3fc3f4862cc5e5
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-09 16:33:51 +0300

    Add TeamIdsAddedToRoomDomainEvent

---
+++ TicTacToeOnline.Domain/DomainEvents/TeamIdsAddedToRoomDomainEvent.cs
@@ -0,0 +1,8 @@
+﻿using TicTacToeOnline.Domain.Common.Interfaces;
+using TicTacToeOnline.Domain.RoomAggregate.ValueObjects;
+using TicTacToeOnline.Domain.TeamAggregate.ValueObjects;
+
+namespace TicTacToeOnline.Domain.DomainEvents
+{
+    public record TeamIdsAddedToRoomDomainEvent(Guid Id, RoomId RoomId, IEnumerable<TeamId> TeamIds) : IDomainEvent;
+}
