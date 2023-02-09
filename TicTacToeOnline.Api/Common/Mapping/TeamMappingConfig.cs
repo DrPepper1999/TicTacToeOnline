eaa5105d7b1c55cb1b59d032ad283dbc5bca6988
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-09 16:33:01 +0300

    Add TeamMappingConfig

---
+++ TicTacToeOnline.Api/Common/Mapping/TeamMappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using Mapster;
+using TicTacToeOnline.Application.Players.Commands.CreatePlayer;
+using TicTacToeOnline.Contracts.Player;
+using TicTacToeOnline.Contracts.Team;
+using TicTacToeOnline.Domain.TeamAggregate;
+
+namespace TicTacToeOnline.Api.Common.Mapping
+{
+    public class TeamMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Team, TeamResponse>()
+                .Map(dest => dest.Id, src => src.Id.Value)
+                .Map(dest => dest.Mark, src => src.Mark.ToString())
+                .Map(dest => dest.PlayerIds, src => src.PlayerIds.Select(x => x.Value));
+        }
+    }
+}
