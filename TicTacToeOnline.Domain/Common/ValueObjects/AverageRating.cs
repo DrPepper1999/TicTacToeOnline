cc352a6b78ac04c143f600b0b0d8d5ed2e911eaf
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-09 16:33:17 +0300

    Delete using

--- TicTacToeOnline.Domain/Common/ValueObjects/AverageRating.cs
+++ TicTacToeOnline.Domain/Common/ValueObjects/AverageRating.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using TicTacToeOnline.Domain.Common.Models;
 
 namespace TicTacToeOnline.Domain.Common.ValueObjects
