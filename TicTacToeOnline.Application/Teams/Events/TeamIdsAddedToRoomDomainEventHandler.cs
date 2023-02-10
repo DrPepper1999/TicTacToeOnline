be4fe8ae4e011572dc043a3928991555217aaf02
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-10 14:46:25 +0300

    Delete roomRepository

--- TicTacToeOnline.Application/Teams/Events/TeamIdsAddedToRoomDomainEventHandler.cs
+++ TicTacToeOnline.Application/Teams/Events/TeamIdsAddedToRoomDomainEventHandler.cs
@@ -8,24 +8,15 @@ namespace TicTacToeOnline.Application.Teams.Events
 {
     public class TeamIdsAddedToRoomDomainEventHandler : INotificationHandler<TeamIdsAddedToRoomDomainEvent>
     {
-        private readonly IRoomRepository _roomRepository;
         private readonly ITeamRepository _teamRepository;
 
-        public TeamIdsAddedToRoomDomainEventHandler(IRoomRepository roomRepository, ITeamRepository teamRepository)
+        public TeamIdsAddedToRoomDomainEventHandler(ITeamRepository teamRepository)
         {
-            _roomRepository = roomRepository;
             _teamRepository = teamRepository;
         }
 
         public async Task Handle(TeamIdsAddedToRoomDomainEvent notification, CancellationToken cancellationToken)
         {
-            var room = await _roomRepository.GetByIdAsync(notification.RoomId.Value, cancellationToken);
-
-            if (room is null)
-            {
-                return;
-            }
-
             var mark = (Mark)1;
             foreach (var teamId in notification.TeamIds)
             {
