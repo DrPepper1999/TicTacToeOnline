359c4610177241295edc026bcc19dce791903de4
Даниил Молчанов <molchanov.daniil2014@yandex.ru>
2023-02-10 14:45:49 +0300

    Add SaveEntitiesAsync

--- TicTacToeOnline.Application/Common/Interfaces/Persistence/IUnitOfWork.cs
+++ TicTacToeOnline.Application/Common/Interfaces/Persistence/IUnitOfWork.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TicTacToeOnline.Application.Common.Interfaces.Persistence
+﻿namespace TicTacToeOnline.Application.Common.Interfaces.Persistence
 {
     public interface IUnitOfWork : IDisposable
     {
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+        Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default(CancellationToken));
     }
+
 }
